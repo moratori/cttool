deaace2f1cfce817a593f08f640187810bba1b1f
moratori <hoko1993@yahoo.co.jp>
2020-04-05 12:39:09 +0900

    テストケースのひな形作成

---
+++ test/test_cttool.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import unittest
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+
+import cttool
+
+
+class TestCTtool(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
