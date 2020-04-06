ea02071f477cc6223d1dd63f1110647255eb2711
moratori <hoko1993@yahoo.co.jp>
2020-04-07 01:24:08 +0900

    fireを用いてコマンドラインアプリ化

--- cttool.py
+++ cttool.py
@@ -2,36 +2,14 @@
 
 import io
 import base64
+import time
 import json
-import argparse
-import requests
 import sys
 import urllib.parse
+import fire
+import requests
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-
-
-def prepare_commandline_arguments():
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("command", choices=CTApp.COMMAND)
-    argparser.add_argument("logserver",
-                           type=str,
-                           help="URL for log server")
-    argparser.add_argument("--timeout", type=int, default=5)
-    argparser.add_argument("--json", type=bool, default=False)
-    argparser.add_argument("--interval",
-                           type=int,
-                           default=15,
-                           help="interval for retrieving data")
-    argparser.add_argument("--startsize",
-                           type=int,
-                           default=None,
-                           help="index of first entry to retrieve, in decimal")
-    argparser.add_argument("--endsize",
-                           type=int,
-                           default=None,
-                           help="index of last entry to retrieve, in decimal")
-    return argparser.parse_args()
 
 
 def get_url(url, timeout, params=dict()):
@@ -47,48 +25,73 @@ def get_url(url, timeout, params=dict()):
         return None
 
 
-class CTApp:
+class Application:
 
-    COMMAND = ["monitor", "roots", "sth"]
-
-    def __init__(self, args):
-        self.cttool = CTtool(args.logserver, args.timeout)
-        self.command = args.command
-        self.interval = args.interval
-        self.json = args.json
-        self.startsize = args.startsize
-        self.endsize = args.endsize
-
-    def summarize_certificates(self, startsize, endsize):
-        for (flag, pem, cert) in self.cttool.get_certificates(startsize,
-                                                              endsize):
-            print("%s\t%d\t%s\t%s\t%s\t%s" % (
-                ("Precert" if flag else "Leafcrt"),
-                cert.serial_number,
-                cert.not_valid_before,
-                cert.not_valid_after,
-                cert.issuer.rfc4514_string(),
-                cert.subject.rfc4514_string()))
-
-    def summarize_certificates_in_json(self, startsize, endsize):
+    def _summarize_certificate(self, logserver, timeout, start, end, jsonflg):
+        ctclient = CTclient(logserver, timeout)
         result = []
-        for (flag, pem, cert) in self.cttool.get_certificates(startsize,
-                                                              endsize):
-            result.append(
-                dict(cert_type=("pre" if flag else "leaf"),
-                     serial=cert.serial_number,
-                     not_valid_before=str(cert.not_valid_before),
-                     not_valid_after=str(cert.not_valid_after),
-                     issuer=cert.issuer.rfc4514_string(),
-                     subject=cert.subject.rfc4514_string(),
-                     pem=pem))
-        print(json.dumps(result))
+        for (flag, pem, cert) in ctclient.get_certificates(start, end):
+            line = dict(cert_type=("precert" if flag else "leafcrt"),
+                        serial=cert.serial_number,
+                        not_valid_before=str(cert.not_valid_before),
+                        not_valid_after=str(cert.not_valid_after),
+                        issuer=cert.issuer.rfc4514_string(),
+                        subject=cert.subject.rfc4514_string(),
+                        pem=pem)
+            result.append(line)
+            if not jsonflg:
+                tmp = line.copy()
+                del tmp["pem"]
+                print("\t".join([str(column) for column in tmp.values()]))
+        if jsonflg:
+            print(json.dumps(result, indent=4, sort_keys=True))
 
-    def start(self):
+    def monitor(self, logserver, timeout=5, interval=10, jsonflg=False,
+                start=None, end=None):
+        ctclient = CTclient(logserver, timeout)
+        if start and end:
+            self._summarize_certificate(logserver, timeout, start, end,
+                                        jsonflg)
+        else:
+            try:
+                while True:
+                    before = ctclient.get_sth()
+                    time.sleep(interval)
+                    after = ctclient.get_sth()
+                    if not (before and "tree_size" in before and
+                            after and "tree_size" in after):
+                        print("unable to get tree_size", file=sys.stderr)
+                        break
+                    else:
+                        start = before["tree_size"]
+                        end = after["tree_size"]
+                        if end > start:
+                            self._summarize_certificate(logserver, timeout,
+                                                        start, end, jsonflg)
+            except KeyboardInterrupt:
+                pass
+
+    def sth(self, logserver, timeout=5):
+        ctclient = CTclient(logserver, timeout)
+        ret = ctclient.get_sth()
+        if ret:
+            print(json.dumps(ret, indent=4, sort_keys=True))
+
+    def logs(self, timeout=5):
+        logs_list = "https://www.gstatic.com/ct/log_list/v2/all_logs_list.json"
+        ret = get_url(logs_list, timeout)
+        if ret and "operators" in ret:
+            for operator in ret["operators"]:
+                if "logs" in operator:
+                    for log in operator["logs"]:
+                        print("%s\n\t%s" % (log["description"],
+                                            log["url"]))
+
+    def roots(self, logserver, timeout=5):
         pass
 
 
-class CTtool:
+class CTclient:
 
     GET_STH = "ct/v1/get-sth"
     GET_ROOTS = "ct/v1/get-roots"
@@ -98,30 +101,27 @@ class CTtool:
         self.logserver = logserver.rstrip("/") + "/"
         self.timeout = timeout
 
-    def get_current_tree_size(self):
+    def get_sth(self):
         url = urllib.parse.urljoin(self.logserver,
-                                   CTtool.GET_STH)
+                                   CTclient.GET_STH)
         ret = get_url(url, self.timeout)
-        if ret is not None:
-            return ret.get("tree_size")
-        print("unable to get tree size", file=sys.stderr)
-        return None
+        if ret is None:
+            print("unable to get tree size", file=sys.stderr)
+        return ret
 
     def get_certificates(self, startsize, endsize):
         url = urllib.parse.urljoin(self.logserver,
-                                   CTtool.GET_ENTRIES)
+                                   CTclient.GET_ENTRIES)
         params = dict(start=startsize, end=endsize)
         ret = get_url(url, self.timeout, params=params)
 
         result = []
-        if ret is not None and ("entries" in ret):
+        if ret and ("entries" in ret):
             for entry in ret["entries"]:
                 try:
                     precert_flag, pem, cert = \
                         self.parse_entry_to_certificate(entry)
-                    if (precert_flag is not None) and \
-                            (pem is not None) and \
-                            (cert is not None):
+                    if precert_flag and pem and cert:
                         result.append((precert_flag, pem, cert))
                 except Exception:
                     continue
@@ -159,12 +159,12 @@ class CTtool:
                     break
 
             data = base64.b64encode(data).decode("utf8")
-            amble = "-----BEGIN CERTIFICATE-----\n%s\n-----END CERTIFICATE-----"
-            pem = (amble % data)
+            dlm = "-----BEGIN CERTIFICATE-----\n%s\n-----END CERTIFICATE-----"
+            pem = (dlm % data)
             cert = x509.load_pem_x509_certificate(pem.encode("utf8"),
                                                   default_backend())
 
-            return pem, cert
+            return data, cert
         except Exception as ex:
             print(str(ex), file=sys.stderr)
             return None, None
@@ -180,10 +180,5 @@ class CTtool:
         return precert_flag, pem, cert
 
 
-def main():
-    args = prepare_commandline_arguments()
-    CTApp(args).start()
-
-
 if __name__ == "__main__":
-    main()
+    fire.Fire(Application)
